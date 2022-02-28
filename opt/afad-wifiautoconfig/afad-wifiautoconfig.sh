28c95fe7085d228c181db7622dab7f4d3246e434
Enes Mücahit Öztürk <78278239+enesmozturk@users.noreply.github.com>
2022-02-28 11:44:32 +0300

    yeni-proje

--- opt/afad-wifiautoconfig/afad-wifiautoconfig.sh
+++ opt/afad-wifiautoconfig/afad-wifiautoconfig.sh
@@ -1,42 +1,48 @@
+if [ -f /etc/NetworkManager/system-connections/Personel.nmconnection ] ; then
+rm /etc/NetworkManager/system-connections/Personel.nmconnection
+else
+echo ok
+fi
+
+if [ -f /etc/NetworkManager/system-connections/Personel_$SUDO_USER.nmconnection ] ; then
+echo ok
+else
 #!/bin/sh
-KIRMIZI_FG=`tput setaf 1`
-YESIL_FG=`tput setaf 2`
-SARI_FG=`tput setaf 3`
-MAVI_FG=`tput setaf 4`
-MOR_FG=`tput setaf 5`
-LILA_FG=`tput setaf 6`
-KIRMIZI_BG=`tput setab 1`
-YESIL_BG=`tput setab 2`
-SARI_BG=`tput setab 3`
-MAVI_BG=`tput setab 4`
-MOR_BG=`tput setab 5`
-LILA_BG=`tput setab 6`
-RESET=`tput sgr0`
-BOLD=`tput bold`
-CIZ=`tput sgr 0 1`
-echo """                      ___       _______    ___       _______  
-                     /   \     |   ____|  /   \     |       \ 
-                    /  ^  \    |  |__    /  ^  \    |  .--.  |
-                   /  /_\  \   |   __|  /  /_\  \   |  |  |  |
-                  /  _____  \  |  |    /  _____  \  |  '--'  |
-                 /__/     \__\ |__|   /__/     \__\ |_______/ 
-                                                              
-                       ____    ____ .______   .__   __. 
-                       \   \  /   / |   _  \  |  \ |  | 
-                        \   \/   /  |  |_)  | |   \|  | 
-                         \      /   |   ___/  |  .    | 
-                          \    /    |  |      |  |\   | 
-                           \__/     | _|      |__| \__| 
-                                                        
-########################################################################														
-#              ${BOLD}${MAVI_FG}AFAD VPN Uygulamasına Hoşgeldiniz!${RESET}                      #
-########################################################################
-################### ${LILA_FG}Giriş yapabilmeniz için;${RESET} ########################### 
--${LILA_FG}Adım 1${RESET} : Kullanıcı Adınızı girecekesiniz
--${LILA_FG}Adım 2${RESET} : Kullanıcı Şifrenizi gireceksiniz
-########################################################################
-***${YESIL_FG} ESP tunnel connected${RESET} *** Yazısını gördüğünüzde VPN bağlantınız aktif oldu. 
-# Bu terminali VPN bağlantınız boyunca ${KIRMIZI_FG}${BOLD}'KAPATMAYIN'${RESET} yoksa bağlantınız kesilir.
-# VPN bağlantınızı ${SARI_FG}sonlandırmak${RESET} için bu terminali kapatın
-"""
-sudo openconnect --protocol=gp merkez.afad.gov.tr --authgroup=GW --servercert pin-sha256:g76Wf4Ae6Axba2I+7HgUfwb8StBuZBXozmgBInIwA4A= 
+echo """
+[connection]
+id=Personel
+uuid=2c99bb39-448c-4af9-98c6-e4f5e7504665
+type=wifi
+autoconnect=false
+permissions=user:$SUDO_USER:;
+
+[wifi]
+mac-address-blacklist=
+mode=infrastructure
+ssid=Personel
+
+[wifi-security]
+key-mgmt=wpa-eap
+
+[802-1x]
+eap=peap;
+identity=$SUDO_USER
+password-flags=2
+phase2-auth=mschapv2
+
+[ipv4]
+dns-search=
+method=auto
+
+[ipv6]
+addr-gen-mode=stable-privacy
+dns-search=
+method=auto
+
+[proxy]
+""" > /etc/NetworkManager/system-connections/Personel_$SUDO_USER.nmconnection
+chmod 600 /etc/NetworkManager/system-connections/Personel_$SUDO_USER.nmconnection
+nmcli connection reload
+fi
+
+
